df1d43599e09fe5c0cb62b6d878b77d69ce4bddd
Dejota Freitas <djxpgs@hotmail.com>
2019-10-17 15:55:27 -0300

    s

---
+++ DJUNGLE-ZARATOM/evento/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from evento.models import Evento, Usuario, Inscricao
+
+# Register your models here.
+admin.site.register(Evento)
+admin.site.register(Usuario)
+admin.site.register(Inscricao)
---
+++ DJUNGLE01/GUIA DJUNGLE.py
@@ -0,0 +1,16 @@
+# INSTALANDO
+# pip install Django==2.0.5
+
+# CRIANDO PROJETO
+# django-admin startproject DJUNGLE01
+
+# INICIANDO SERVIDOR
+# python manage.py runserver
+# OU
+# python manage.py runserver 80
+
+# SINCRONIZAR BANCO DE DADOS
+# python manage.py migrate --run-syncdb
+
+# CRIAR APLICACAO
+# python manage.py startapp geral
